6dad28a603c3cfd68866e8ee386a855ddaa7b01d
Miguel Angel Ybañez Esquerre <arcangel080902@gmail.com>
2024-09-26 01:08:49 -0500

    Last commit of the day

--- app/src/main/java/com/kanarian/androidclass/imccalculator/IMCCalculatorActivity.kt
+++ app/src/main/java/com/kanarian/androidclass/imccalculator/IMCCalculatorActivity.kt
@@ -9,6 +9,7 @@ import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import com.kanarian.androidclass.R
 
@@ -16,12 +17,22 @@ class IMCCalculatorActivity : AppCompatActivity() {
 
     private var isCardSelected:Boolean =true
     private var lastCardSelected:Int = 0
+    private var currentWeight:Int = 60
+    private var currentAge:Int = 22
 
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
 
     private lateinit var tvHeight: TextView
     private lateinit var rsHeight: RangeSlider
+
+    private lateinit var btnSubtractWeight: FloatingActionButton
+    private lateinit var btnPlusWeight: FloatingActionButton
+    private lateinit var tvWeight: TextView
+
+    private lateinit var btnSubtractAge: FloatingActionButton
+    private lateinit var btnPlusAge: FloatingActionButton
+    private lateinit var tvAge: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +50,16 @@ class IMCCalculatorActivity : AppCompatActivity() {
     }
 
     private fun initComponents() {
-        viewMale = findViewById<CardView>(R.id.viewMale)
-        viewFemale = findViewById<CardView>(R.id.viewFemale)
-        tvHeight = findViewById<TextView>(R.id.tvHeight)
-        rsHeight = findViewById<RangeSlider>(R.id.rsHeight)
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight)
+        btnSubtractWeight = findViewById(R.id.btnSubtractWeight)
+        btnPlusWeight = findViewById(R.id.btnPlusWeight)
+        tvWeight = findViewById(R.id.tvWeight)
+        btnSubtractAge = findViewById(R.id.btnSubtractAge)
+        btnPlusAge = findViewById(R.id.btnPlusAge)
+        tvAge = findViewById(R.id.tvAge)
     }
 
     private fun initListeners() {
@@ -59,6 +76,30 @@ class IMCCalculatorActivity : AppCompatActivity() {
             val result = df.format(value)
             tvHeight.text = "${result} cm"
         }
+        btnPlusWeight.setOnClickListener {
+            currentWeight += 1
+            setWeight()
+        }
+        btnSubtractWeight.setOnClickListener {
+            currentWeight -= 1
+            setWeight()
+        }
+        btnPlusAge.setOnClickListener {
+            currentAge += 1
+            setAge()
+        }
+        btnSubtractAge.setOnClickListener {
+            currentAge -= 1
+            setAge()
+        }
+    }
+
+    private fun setAge() {
+        tvAge.text = currentAge.toString()
+    }
+
+    private fun setWeight() {
+        tvWeight.text = currentWeight.toString()
     }
 
     private fun changeGender(card:Int){
@@ -87,5 +128,7 @@ class IMCCalculatorActivity : AppCompatActivity() {
 
     private fun initUI() {
         setGenderColor()
+        setWeight()
+        setAge()
     }
 }
